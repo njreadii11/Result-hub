475ec790d7b558dd9b85dc3a4c07dafbd67473c2
njreadii11 <njraadii74298@gmail.com>
2026-02-26 23:36:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,76 @@
 function checkResult() {
 
-    var roll = document.getElementById("roll").value;
-    var reg = document.getElementById("reg").value;
-    var output = document.getElementById("output");
+    var roll = document.getElementById("roll").value.trim();
+    var reg = document.getElementById("reg").value.trim();
+    var error = document.getElementById("error");
+    var result = document.getElementById("result");
 
-    // Example student database
-    if (roll === "1316" && reg === "1104-R24-1316") {
+    error.innerHTML = "";
+    result.innerHTML = "";
 
-        output.innerHTML = `
-            <h3>RESULT DETAILS</h3>
-            Name: Aadil Shabir Najar <br>
-            Course: Diploma in Electronics & Communication <br>
-            Result: PASS <br>
-            SGPA: <span style="color:green; font-weight:bold;">9.56</span>
-        `;
-    } 
-    else {
-        output.innerHTML = "<span style='color:red;'>Invalid Roll Number or Registration Number</span>";
+    // Student Database
+    var students = [
+        {
+            roll: "1104-25N3-48232",
+            reg: "11-04-R24-1316",
+            name: "Aadil Shabir Najar",
+            course: "Diploma in Electronics & Communication",
+            sgpa: "9.56",
+            status: "PASS"
+        },
+        {
+            roll: "2025-001",
+            reg: "REG-2025-001",
+            name: "Test Student",
+            course: "Computer Engineering",
+            sgpa: "9.44",
+            status: "PASS"
+        }
+    ];
+
+    var found = false;
+
+    for (var i = 0; i < students.length; i++) {
+        if (roll === students[i].roll && reg === students[i].reg) {
+
+            result.innerHTML = `
+                <table>
+                    <tr>
+                        <th colspan="2">RESULT DETAILS</th>
+                    </tr>
+                    <tr>
+                        <td>Name</td>
+                        <td>${students[i].name}</td>
+                    </tr>
+                    <tr>
+                        <td>Roll Number</td>
+                        <td>${students[i].roll}</td>
+                    </tr>
+                    <tr>
+                        <td>Registration Number</td>
+                        <td>${students[i].reg}</td>
+                    </tr>
+                    <tr>
+                        <td>Course</td>
+                        <td>${students[i].course}</td>
+                    </tr>
+                    <tr>
+                        <td>Result</td>
+                        <td>${students[i].status}</td>
+                    </tr>
+                    <tr>
+                        <td>SGPA</td>
+                        <td class="sgpa">${students[i].sgpa}</td>
+                    </tr>
+                </table>
+            `;
+
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        error.innerHTML = "Invalid Roll Number or Registration Number";
     }
 }
